4e7b39c5238a214eece7642cc672ee7db10bcd28
prasun <prasun.1928@gmail.com>
2025-08-05 07:59:48 +0530

    ready for deployment

--- server/server.js
+++ server/server.js
@@ -2,25 +2,28 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './config/db.js';
+
 import authRoutes from './routes/authRoutes.js';
 import todoRoutes from './routes/todoRoutes.js';
 
-
 dotenv.config();
-
-//database connection
 connectDB();
 
 const app = express();
 
-// Middleware 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
+
+
 app.use(express.json());
 
-// API Routes 
 app.use('/api/auth', authRoutes);
 app.use('/api/todolists', todoRoutes);
 
+
 const PORT = process.env.PORT || 5001;
 
-app.listen(PORT, () => console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
